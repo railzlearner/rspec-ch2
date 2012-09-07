9b547e91cacd781cb191cbb9a3a9a4f3257ee087
Amit Nangare <anangare@cisco.com>
2012-09-07 16:07:34 -0700

    RSpec book chapter 2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecCh2::Application.config.secret_token = '764f5116a61f1867032dd8c36a71550851c99f7846153fd92d50e456c274b815df76e29bea9f4f1b2e70a7d06699d29a10cdde1569c03d77bca023f8ad5066e5'
